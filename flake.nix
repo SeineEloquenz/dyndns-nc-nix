b87b132400b771b750055178ac3bb2cea0844635
Alexander Linder <seine@eloque.nz>
2024-05-27 17:01:31 +0200

    Fix container version

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
     containers.hass-addon = pkgs.dockerTools.buildLayeredImage {
       name = "ghcr.io/seineeloquenz/dyndns-netcup";
-      tag = "latest";
+      tag = "v0.1.1";
       contents = [
         dyndns-nc
       ];
